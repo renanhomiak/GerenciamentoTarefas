9031a6ea1bfdc2d409908d1aaf50e38bcc3f6974
renanhomiak <renanhomiakg@gmail.com>
2026-03-24 16:16:11 -0300

    segundo commit

---
+++ src/main/java/com/homiak/ex10/repository/TarefaRepository.java
@@ -0,0 +1,7 @@
+package com.homiak.ex10.repository;
+
+import com.homiak.ex10.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
+}
